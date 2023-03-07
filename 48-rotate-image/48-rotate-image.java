a0f280f8b9f7e7cae91fca0ba1e1a3ae095a82fe
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-03-07 11:42:59 +0530

    Time: 0 ms (100.00%), Space: 42.4 MB (23.09%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -11,22 +11,15 @@ class Solution {
         }
     }
     public void rotate(int[][] matrix) {
-        int[][] res = new int[matrix.length][matrix[0].length];
-        
         for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                res[i][j] = matrix[j][i]; 
+            for(int j=i; j<matrix[0].length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
-        
         for(int i=0; i<matrix.length; i++){
-            reverse(res, i, matrix[0].length-1);
-        }
-        
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                matrix[i][j] = res[i][j]; 
-            }
+            reverse(matrix, i, matrix[0].length-1);
         }
     }
 }
