8e72ef7a6856a31570760c186691e7a2c36096a8
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-03-08 09:56:22 +0530

    Time: 4 ms (88.04%), Space: 59.7 MB (80.54%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,25 +1,47 @@
 class Solution {
-    private void swap(int[] nums, int i, int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    public int findDuplicate(int[] nums) {
-        int left = 0;
+    /*
+    * Using Cycle Sort- Modifying the given Array
+    */
+//     private void swap(int[] nums, int i, int j){
+//         int temp = nums[i];
+//         nums[i] = nums[j];
+//         nums[j] = temp;
+//     }
+//     public int findDuplicate(int[] nums) {
+//         int left = 0;
         
-        while(left < nums.length){
-            if(nums[left] == left+1){
-                left++;
-            }
-            else{
-                if(nums[nums[left]-1] == nums[left]){
-                    return nums[left];
-                }
-                else{
-                    swap(nums, left, nums[left]-1);
-                }
-            }
+//         while(left < nums.length){
+//             if(nums[left] == left+1){
+//                 left++;
+//             }
+//             else{
+//                 if(nums[nums[left]-1] == nums[left]){
+//                     return nums[left];
+//                 }
+//                 else{
+//                     swap(nums, left, nums[left]-1);
+//                 }
+//             }
+//         }
+//         return -1;
+//     }
+    
+    public int findDuplicate(int[] nums) {
+        int slow = nums[0];
+        int fast = nums[0];
+        
+        do{
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }while(slow != fast);
+        
+        fast = nums[0];
+        
+        while(slow != fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return -1;
+        return slow;
     }
+    
 }
