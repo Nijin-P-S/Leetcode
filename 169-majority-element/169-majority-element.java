c2aa3ff810cefccd69742905db06c2468b50681d
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-03-10 09:05:32 +0530

    Time: 1 ms (99.90%), Space: 45.9 MB (64.17%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int ele = 0;
+        
+        for(int val : nums){
+            if(count == 0){
+                ele = val;
+            }
+            if(ele == val){
+                count++;
+            }
+            else{
+                count--;
+            }
+        }
+        return ele;
+    }
+}
