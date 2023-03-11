11a486e84982d6a098176f08bf97fe22204f7b72
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-03-11 10:05:20 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (38.31%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,32 +1,43 @@
 class Solution {
-    private int paths(int m, int n, int i, int j, int[][] dp){
-        if(i==m-1 && j==n-1){
-            return 1;
-        }
+    //Using DP solution
+//     private int paths(int m, int n, int i, int j, int[][] dp){
+//         if(i==m-1 && j==n-1){
+//             return 1;
+//         }
         
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        } 
-        int numPaths = 0;
-        int downPath = 0;
-        int rightPath = 0;
-        if(i+1 < m){
-            downPath = paths(m, n, i+1, j, dp);
-        }
-        if(j+1 < n){
-            rightPath = paths(m, n, i, j+1, dp);
-        }
-        numPaths = downPath+rightPath;
-        return dp[i][j] = numPaths;
-    }
-    public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+//         if(dp[i][j] != -1){
+//             return dp[i][j];
+//         } 
+//         int numPaths = 0;
+//         int downPath = 0;
+//         int rightPath = 0;
+//         if(i+1 < m){
+//             downPath = paths(m, n, i+1, j, dp);
+//         }
+//         if(j+1 < n){
+//             rightPath = paths(m, n, i, j+1, dp);
+//         }
+//         numPaths = downPath+rightPath;
+//         return dp[i][j] = numPaths;
+//     }
+//     public int uniquePaths(int m, int n) {
+//         int[][] dp = new int[m][n];
         
-        for(int i=0; i<m; i++){
-            for(int j=0; j<n; j++)
-                dp[i][j] = -1;
-        }
+//         for(int i=0; i<m; i++){
+//             for(int j=0; j<n; j++)
+//                 dp[i][j] = -1;
+//         }
         
-        return paths(m, n, 0, 0, dp);
-    }
+//         return paths(m, n, 0, 0, dp);
+//     }
+      public int uniquePaths(int m, int n){
+          int N = m+n-2;
+          int r = m-1;
+          double res = 1;
+          
+          for(int i=1; i<=r; i++){
+              res = res*(N-r+i)/i;
+          }
+          return (int) res;
+      }
 }
