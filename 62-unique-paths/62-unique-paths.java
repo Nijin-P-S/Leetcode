e36b891b269ba9a4eb7c17aa8308353dd1ae43bf
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-03-11 09:54:25 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (19.60%) - LeetHub

---
+++ 62-unique-paths/62-unique-paths.java
@@ -0,0 +1,32 @@
+class Solution {
+    private int paths(int m, int n, int i, int j, int[][] dp){
+        if(i==m-1 && j==n-1){
+            return 1;
+        }
+        
+        if(dp[i][j] != -1){
+            return dp[i][j];
+        } 
+        int numPaths = 0;
+        int downPath = 0;
+        int rightPath = 0;
+        if(i+1 < m){
+            downPath = paths(m, n, i+1, j, dp);
+        }
+        if(j+1 < n){
+            rightPath = paths(m, n, i, j+1, dp);
+        }
+        numPaths = downPath+rightPath;
+        return dp[i][j] = numPaths;
+    }
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++)
+                dp[i][j] = -1;
+        }
+        
+        return paths(m, n, 0, 0, dp);
+    }
+}
