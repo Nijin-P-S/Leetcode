9ec59410b15d1d3132a7029e91fcc0c02f55234d
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-03-13 09:43:53 +0530

    Time: 1 ms (98.50%), Space: 45.2 MB (70.77%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -10,19 +10,39 @@
  * }
  */
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        HashSet<ListNode> inter = new HashSet<>();
+    /*
+    * HashSet Approach
+    */
+//     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+//         HashSet<ListNode> inter = new HashSet<>();
         
-        while(headA != null){
-            inter.add(headA);
-            headA = headA.next;
-        }
+//         while(headA != null){
+//             inter.add(headA);
+//             headA = headA.next;
+//         }
         
-        while(headB != null){
-            if(inter.contains(headB))
-                return headB;
-            headB = headB.next;
+//         while(headB != null){
+//             if(inter.contains(headB))
+//                 return headB;
+//             headB = headB.next;
+//         }
+//         return null;
+//     }
+    
+    /*
+    * Optimal approach using two dummy pointers
+    */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB){
+        if(headA == null || headB == null)
+            return null;
+        
+        ListNode a = headA;
+        ListNode b = headB;
+        
+        while(a != b){
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
         }
-        return null;
+        return a;
     }
 }
