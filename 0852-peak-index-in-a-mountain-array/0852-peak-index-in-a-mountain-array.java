a93169024cd17f4e9857dc79597c6a2bb53665a6
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-03-20 21:57:20 +0530

    Time: 0 ms (100.00%), Space: 58.8 MB (75.52%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -1,10 +1,8 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
-        int left = 0, right = arr.length-1;
-        
+        int left = 0, right = arr.length-1;       
         while(left <= right){
-            int mid = left + (right-left)/2;
-            
+            int mid = left + (right-left)/2;        
             if(arr[mid] > arr[mid+1])
                 right = mid-1;
             else
