e9b90d5bafae9d18bc8507740b54be5e0473315d
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-03-21 22:44:05 +0530

    Time: 1 ms (48.71%), Space: 43.4 MB (25.36%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.java
@@ -0,0 +1,27 @@
+class Solution {
+
+
+    private void cycleSort(int[] nums){
+        int i=0;
+        
+        while(i < nums.length){
+            if(nums[i] != i && nums[i] < nums.length){
+                int temp = nums[nums[i]];
+                nums[nums[i]] = nums[i];
+                nums[i] = temp;
+            }
+            else{
+                i++;
+            }
+        }
+    }
+    public int missingNumber(int[] nums) {
+        cycleSort(nums);
+        
+        for(int i=0; i<nums.length; i++){
+            if(i != nums[i])
+                return i;
+        }
+        return nums.length;
+    }
+}
