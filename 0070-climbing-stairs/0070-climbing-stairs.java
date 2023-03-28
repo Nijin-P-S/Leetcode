6230001e99530781728c8019afab5db5cc74ae46
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-03-28 21:11:16 +0530

    Time: 0 ms (100.00%), Space: 39.5 MB (26.70%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,22 +1,7 @@
 class Solution {
-    public int climbStairs(int n, int[] count){
-        if(n==0)
-           {
-               return count[0] = 1;
-           }
-        if(n<0)
-            return 0;
-        if(count[n] != -1)
-            return count[n];
-        return count[n] = climbStairs(n-1, count) + climbStairs(n-2, count);
-        
-    }
     public int climbStairs(int n) {
-        if(n<=1)
-            return n;
         int prev = 1;
-        int curr = 1;
-        
+        int curr = 1;   
         for(int i=2; i<=n; i++){
             int temp = prev;
             prev = curr;
