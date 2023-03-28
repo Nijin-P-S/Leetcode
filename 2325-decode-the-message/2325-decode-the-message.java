b13f14fdffa691aa5f5df6a7f4fcd5edcd721de1
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-03-28 20:12:29 +0530

    Time: 9 ms (55.96%), Space: 42.4 MB (64.22%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String decodeMessage(String key, String message) {
+        HashMap<Character, Character> map = new HashMap<>();
+        int ind = 0;
+        String alpha = "abcdefghijklmnopqrstuvwxyz";
+        
+        for(int i=0; i<key.length(); i++){
+            if(key.charAt(i) == ' '){
+                map.put(' ', ' ');
+            }
+            else if(map.get(key.charAt(i)) == null){
+                map.put(key.charAt(i), alpha.charAt(ind));
+                ind++;
+            }
+        }
+        
+        StringBuilder ans = new StringBuilder();
+        for(int i =0; i<message.length(); i++){
+            if(message.charAt(i) == ' '){
+                ans.append(' ');
+                continue;
+            }
+            ans.append(map.get(message.charAt(i)));
+        }
+        
+        return ans.toString();
+    }
+}
