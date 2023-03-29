899eec9b2baa94b24b2a82cc4e6f558517136cb1
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-03-29 23:08:28 +0530

    Time: 1 ms (94.30%), Space: 43.1 MB (18.89%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -4,26 +4,25 @@ class Solution {
         nums[i] = nums[j];
         nums[j] = temp;
     }
-    private void permute(int[] nums, int ind, List<List<Integer>> ans, List<Integer> cur){
+    private void permute(int[] nums, int ind, List<List<Integer>> ans){
         if(ind == nums.length-1){
-            cur.add(nums[ind]);
-            ans.add(new ArrayList<>(cur));
-            cur.remove(cur.size()-1);
+            List<Integer> cur = new ArrayList<>();
+            for(int val : nums){
+                cur.add(val);
+            }
+            ans.add(cur);
             return;
         }
         
         for(int i=ind; i<nums.length; i++){
             swap(nums, ind, i);
-            cur.add(nums[ind]);
-            permute(nums, ind+1, ans, cur);
-            cur.remove(cur.size()-1);
+            permute(nums, ind+1, ans);
             swap(nums, ind, i);
         }
     }
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>();
-        List<Integer> cur = new ArrayList<>();
-        permute(nums, 0, ans, cur);
+        permute(nums, 0, ans);
         return ans;
     }
 }
