a6d4a145e21ef5dc1a94397e2ef5fae118ddc505
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-03-31 15:15:01 +0530

    Time: 0 ms (100.00%), Space: 43.6 MB (26.72%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.java
@@ -0,0 +1,25 @@
+class Solution {
+    private void floodFill(int[][] image, int sr, int sc, int color, int ori){
+        if(image[sr][sc] != ori || image[sr][sc] == color){
+            return;
+        }
+        image[sr][sc] = color;
+        
+        if(sr+1 < image.length && image[sr+1][sc] == ori){
+            floodFill(image, sr+1, sc, color, ori);
+        }
+        if(sr-1 >=0 && image[sr-1][sc] == ori){
+            floodFill(image, sr-1, sc, color, ori);
+        }
+        if(sc+1 < image[0].length && image[sr][sc+1] == ori){
+            floodFill(image, sr, sc+1, color, ori);
+        }
+        if(sc-1 >= 0 && image[sr][sc-1] == ori){
+            floodFill(image, sr, sc-1, color, ori);
+        }
+    }
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        floodFill(image, sr, sc, color, image[sr][sc]);
+        return image;
+    }
+}
