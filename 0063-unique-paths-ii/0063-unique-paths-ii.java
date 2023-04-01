4f727f165f7987d8c07642d268ef127b39421ee2
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-04-01 09:45:53 +0530

    Time: 0 ms (100.00%), Space: 40.4 MB (83.62%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -1,28 +1,40 @@
 class Solution {
-    private int uniquePaths(int[][] obstacleGrid, int i, int j, int[][] dp){
-        if(i == 0 && j == 0)
-            return 1;
-        if(i < 0 || j < 0)
-            return 0;
-        if(obstacleGrid[i][j] == 1)
-            return 0;
+//     private int uniquePaths(int[][] obstacleGrid, int i, int j, int[][] dp){
+//         if(i == 0 && j == 0)
+//             return 1;
+//         if(i < 0 || j < 0)
+//             return 0;
+//         if(obstacleGrid[i][j] == 1)
+//             return 0;
         
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        int left = uniquePaths(obstacleGrid, i, j-1, dp);
-        int top = uniquePaths(obstacleGrid, i-1, j, dp);
-        return dp[i][j] = left + top;
-    }
+//         if(dp[i][j] != -1)
+//             return dp[i][j];
+//         int left = uniquePaths(obstacleGrid, i, j-1, dp);
+//         int top = uniquePaths(obstacleGrid, i-1, j, dp);
+//         return dp[i][j] = left + top;
+//     }
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
         if(obstacleGrid[0][0] == 1 || obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1] == 1)
             return 0;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
         int[][] dp = new int[obstacleGrid.length][obstacleGrid[0].length];
         
-        for(int i=0; i<obstacleGrid.length; i++){
-            for(int j=0; j<obstacleGrid[0].length; j++)
-                dp[i][j] = -1;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(obstacleGrid[i][j] == 1){
+                    dp[i][j] = 0;
+                }
+                else if(i == 0 && j == 0)
+                    dp[i][j] = 1;
+                else{
+                    int left = j>0 ? dp[i][j-1] : 0;
+                    int top = i>0 ? dp[i-1][j] : 0;
+                    dp[i][j] = left + top;
+                }
+            }
         }
         
-        return uniquePaths(obstacleGrid, obstacleGrid.length-1, obstacleGrid[0].length-1, dp);
+        return dp[m-1][n-1];
     }
 }
