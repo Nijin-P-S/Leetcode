0999c96c6ef9ee441314299941f952248a5e0c30
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-04-02 18:30:31 +0530

    Time: 4 ms (54.14%), Space: 41.9 MB (73.12%) - LeetHub

---
+++ 0120-triangle/0120-triangle.java
@@ -0,0 +1,23 @@
+class Solution {
+    private int minTotal(List<List<Integer>> triangle, int n, int ind, int row, int[][] dp){
+        if(row == n-1){
+            return triangle.get(row).get(ind);
+        }
+        if(dp[row][ind] != -1)
+            return dp[row][ind];
+        int min = Integer.MIN_VALUE;
+        int bot = minTotal(triangle, n, ind, row+1, dp) + triangle.get(row).get(ind);
+        int dg = minTotal(triangle, n, ind+1, row+1, dp)  + triangle.get(row).get(ind);
+        
+        return dp[row][ind] = min = Math.min(bot, dg);
+    }
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[][] dp = new int[n][triangle.get(n-1).size()];
+        for(int i=0; i<n; i++){
+            for(int j=0; j<triangle.get(n-1).size(); j++)
+                dp[i][j] = -1;
+        }
+        return minTotal(triangle, n, 0, 0, dp);
+    }
+}
