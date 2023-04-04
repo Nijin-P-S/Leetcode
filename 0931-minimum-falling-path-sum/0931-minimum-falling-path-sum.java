df231f93e3104ba4549e2f8fd704f39c47adb12c
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-04-04 16:10:17 +0530

    Time: 3 ms (69.04%), Space: 43.5 MB (22.81%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,19 +1,4 @@
 class Solution {
-    
-//     private int minFall(int[][] matrix, int i, int j, int row, int col, int[][] dp){
-//         if(i== 0)
-//             return matrix[i][j];
-//         if(dp[i][j] != -1 )
-//             return dp[i][j];
-//         int min = Integer.MAX_VALUE;
-//         for(int k=-1; k<=1; k++){
-//             if(j+k >= 0 && j+k < col){
-//                 min = Math.min(min, matrix[i][j] + minFall(matrix, i-1, j+k, row, col, dp));
-//             }
-//         }
-//         return dp[i][j] = min;
-//     }
-    
     public int minFallingPathSum(int[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
