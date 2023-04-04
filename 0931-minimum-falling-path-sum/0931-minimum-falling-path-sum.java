8476f939152cd74e3607ccb36a8455197db09a11
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-04-04 16:10:04 +0530

    Time: 3 ms (69.04%), Space: 42.8 MB (72.63%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.java
@@ -1,31 +1,43 @@
 class Solution {
     
-    private int minFall(int[][] matrix, int i, int j, int row, int col, int[][] dp){
-        if(i== 0)
-            return matrix[i][j];
-        if(dp[i][j] != -1 )
-            return dp[i][j];
-        int min = Integer.MAX_VALUE;
-        for(int k=-1; k<=1; k++){
-            if(j+k >= 0 && j+k < col){
-                min = Math.min(min, matrix[i][j] + minFall(matrix, i-1, j+k, row, col, dp));
-            }
-        }
-        return dp[i][j] = min;
-    }
+//     private int minFall(int[][] matrix, int i, int j, int row, int col, int[][] dp){
+//         if(i== 0)
+//             return matrix[i][j];
+//         if(dp[i][j] != -1 )
+//             return dp[i][j];
+//         int min = Integer.MAX_VALUE;
+//         for(int k=-1; k<=1; k++){
+//             if(j+k >= 0 && j+k < col){
+//                 min = Math.min(min, matrix[i][j] + minFall(matrix, i-1, j+k, row, col, dp));
+//             }
+//         }
+//         return dp[i][j] = min;
+//     }
     
     public int minFallingPathSum(int[][] matrix) {
         int row = matrix.length;
         int col = matrix[0].length;
         int[][] dp = new int[row][col];
-        for(int i=0; i<row; i++){
-            for(int j=0; j<col; j++)
-                dp[i][j] = -1;
+        for(int i=0; i<col; i++){
+                dp[0][i] = matrix[0][i];
         }
+        
+        for(int i=1; i<row; i++){
+            for(int j=0; j<col; j++){
+                int min = Integer.MAX_VALUE;
+                 for(int k=-1; k<=1; k++){
+                    if(j+k >= 0 && j+k < col){
+                        min = Math.min(min, matrix[i][j] + dp[i-1][j+k]);
+                    }
+                }
+                dp[i][j] = min;
+            }
+        }
+        
         int min = Integer.MAX_VALUE;
         
         for(int i=0; i<col; i++){
-             min = Math.min(min, minFall(matrix, row-1, i, row, col, dp));
+             min = Math.min(min, dp[row-1][i]);
         }
         return min;
     }
