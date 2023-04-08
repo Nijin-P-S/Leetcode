5e95b915ceeaa4551ce7b3d78be6739119568aa8
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-04-08 17:26:17 +0530

    Time: 2 ms (21.32%), Space: 42.4 MB (34.18%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,12 +14,16 @@
  * }
  */
 class Solution {
+    HashMap<TreeNode, Integer> maxLen = new HashMap<>();
     public int diam(TreeNode root){
         if(root == null)
             return 0;
+        if(maxLen.get(root) != null)
+            return maxLen.get(root);
         int left = 1+diam(root.left);
         int right = 1+diam(root.right);
         
+        maxLen.put(root,Math.max(left, right));
         return Math.max(left, right);
         
     }
