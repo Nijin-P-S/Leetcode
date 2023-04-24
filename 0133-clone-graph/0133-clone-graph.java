bb6e1ef9f17095580a04accfe9a769712bbcf4d7
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-04-24 21:18:36 +0530

    Time: 25 ms (65.23%), Space: 42.9 MB (39.66%) - LeetHub

---
+++ 0133-clone-graph/0133-clone-graph.java
@@ -0,0 +1,56 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+
+    public void neighborMapping(Node node, HashMap<Node, Node> map, int[] visited){
+        if(node == null || visited[node.val] == 1)
+            return;
+        
+        visited[node.val] = 1;
+        List<Node> neighborList = new ArrayList<>();
+        for(Node nei : node.neighbors){
+            neighborList.add(map.get(nei));
+            neighborMapping(nei, map, visited);
+        }
+        Node cur = map.get(node);
+        cur.neighbors = neighborList;
+    }
+    
+    public void mapping(Node node, HashMap<Node, Node> map, int[] visited){
+        if(node == null)
+            return;
+        if(visited[node.val] == 1)
+            return;
+        visited[node.val] = 1;
+        map.put(node, new Node(node.val));
+        for(Node neighbor : node.neighbors){
+                mapping(neighbor, map, visited);
+        }
+    }
+    public Node cloneGraph(Node node) {
+        HashMap<Node, Node> map = new HashMap<>();
+        int[] visited = new int[101];
+        mapping(node, map, visited);
+        Arrays.fill(visited, 0);
+        neighborMapping(node, map, visited);
+        return map.get(node);
+    }
+}
