31b39c54b8b2b9a9311b4f03dff238c6744edf55
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-04-25 23:31:47 +0530

    Time: 2 ms (50.23%), Space: 50.6 MB (82.40%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        HashSet<Integer> zeros = new HashSet<>();
+        int zeroCount = 0;
         
         int[] ans = new int[nums.length];
         int pro = 1;
@@ -9,19 +9,19 @@ class Solution {
             if(nums[i] != 0)
                 pro*=nums[i];
             else{
-                zeros.add(i);
+                zeroCount++;
             }
         }
         
         for(int i=0; i<nums.length; i++){
             if(nums[i] != 0){
-                if(zeros.size() == 0)
+                if(zeroCount == 0)
                     ans[i] = pro/nums[i];
                 else
                     ans[i] = 0;
             }
             else{
-                if(zeros.size() > 1)
+                if(zeroCount > 1)
                     ans[i] = 0;
                 else
                     ans[i] = pro;
