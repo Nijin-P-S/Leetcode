779e093abf5018ef14dcbb4bd26f7eeb25ed24bc
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-04-28 23:05:47 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (65.08%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -3,31 +3,31 @@ class Solution {
         List<Integer> ans = new ArrayList<>();
         int m = matrix.length;
         int n = matrix[0].length;
-        int count = matrix.length%2 == 0 ? matrix.length/2 : matrix.length/2+1;
-        int totCount = matrix.length*matrix[0].length;
+        
+        int totCount = m*n;
         int numCount = 0;
-        int index = 0;
+        
+        int delta = 0;
         while(numCount < totCount){
-            for(int i=index; numCount<totCount && i<n-index; i++){
+            for(int i=delta; numCount<totCount && i<n-delta; i++){
                 numCount++;
-                ans.add(matrix[index][i]);
+                ans.add(matrix[delta][i]);
             }        
-            for(int i=index+1; numCount<totCount && i<m-index; i++){
+            for(int i=delta+1; numCount<totCount && i<m-delta; i++){
                 numCount++;
-                ans.add(matrix[i][n-index-1]);
+                ans.add(matrix[i][n-delta-1]);
             }
                 
-            for(int i=n-index-2;numCount<totCount && i>index; i--){
+            for(int i=n-delta-2;numCount<totCount && i>delta; i--){
                 numCount++;
-                ans.add(matrix[m-index-1][i]);
+                ans.add(matrix[m-delta-1][i]);
             }
                 
-            for(int i=m-index-1;numCount<totCount && i>index; i--){
+            for(int i=m-delta-1;numCount<totCount && i>delta; i--){
                 numCount++;
-                ans.add(matrix[i][index]);
+                ans.add(matrix[i][delta]);
             }
-            count--;
-            index++;
+            delta++;
         }
         return ans;
     }
