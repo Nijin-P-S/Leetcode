3ee10f78f52dd3e4052b8a100afa8128789d1e26
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-05-05 11:53:41 +0530

    Time: 4 ms (73.41%), Space: 40.7 MB (59.99%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,23 +1,20 @@
 class Solution {
+    public int helper(String s, int left, int right){
+        int count = 0;
+        while(left>=0 && right < s.length() && s.charAt(left) == s.charAt(right))              {
+                count++;
+                left--;
+                right++;
+            }
+        return count;
+    }
     public int countSubstrings(String s) {
         int count = 0;
         
         for(int i=0; i<s.length(); i++){
             int left = i, right = i;
-            while(left>=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-                count++;
-                left--;
-                right++;
-            }
-            if(i+1 < s.length() && s.charAt(i) == s.charAt(i+1)){
-                left = i;
-                right = i+1;
-                while(left>=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
-                    count++;
-                    left--;
-                    right++;
-                }
-            }
+            count+=helper(s, i, i);
+            count+=helper(s, i, i+1);
         }
         return count;
     }
