c3621b85b0e66c8c28ce5f9b86b0e63d16f5803d
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-05-05 11:51:03 +0530

    Time: 8 ms (47.77%), Space: 40.9 MB (50.64%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,22 +1,21 @@
 class Solution {
-    public boolean checkPalindrome(String s, int i, int j){
-        int left = i, right = j;
-        
-        while(left < right){
-            if(s.charAt(left) != s.charAt(right))
-                return false;
-            left++;
-            right--;
-        }
-        return true;
-    }
     public int countSubstrings(String s) {
         int count = 0;
         
         for(int i=0; i<s.length(); i++){
-            for(int j=i; j<s.length(); j++){
-                if(checkPalindrome(s, i, j)){
+            int left = i, right = i;
+            while(left>=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+                count++;
+                left--;
+                right++;
+            }
+            if(i+1 < s.length() && s.charAt(i) == s.charAt(i+1)){
+                left = i;
+                right = i+1;
+                while(left>=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
                     count++;
+                    left--;
+                    right++;
                 }
             }
         }
