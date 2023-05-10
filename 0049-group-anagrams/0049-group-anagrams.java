1e6534e670ecab14a2a3b8eb37f3acb99fc7f796
Nijin <101330853+Nijin-P-S@users.noreply.github.com>
2023-05-10 09:51:43 +0530

    Time: 44 ms (10.39%), Space: 47.6 MB (5.49%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<
+            List<String>> groupAnagrams(String[] strs) {
+        HashMap<String, List<String>> anag = new HashMap<>();
+        
+        for(String s : strs){
+            int[] cur = new int[26];
+            for(int i=0; i<s.length(); i++){
+                cur[s.charAt(i)-'a']++;
+            }
+            StringBuilder key = new StringBuilder();
+            for(int i=0; i<26; i++){
+                key.append(cur[i]+",");
+            }
+            if(!anag.containsKey(key.toString())){
+                anag.put(key.toString(), new ArrayList<>());
+            }
+            List<String> list = anag.get(key.toString());
+            list.add(s);
+            anag.put(key.toString(), list);
+        }
+        
+        List<List<String>> ans = new ArrayList<>();
+        ans.addAll(anag.values());
+        
+        return ans;
+    }
+}
